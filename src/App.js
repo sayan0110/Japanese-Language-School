595142c9f8879228984d7e80d1c470bcce1dfd95
SAYAN MONDAL <smondal254842@gmail.com>
2024-04-04 17:50:43 +0530

    popup commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import NavMenu from './components/NavMenu';
-import CardSlider from './components/CardSlider';
+import React from "react";
+import "./App.css";
+import NavMenu from "./components/NavMenu";
+import CardSlider from "./components/CardSlider";
+import Popup from "./components/Popup/Popup";
 
 export default function App() {
-    return (
-        <>
-            <NavMenu />
-            <CardSlider />
-        </>
-    );
+  return (
+    <>
+      <NavMenu />
+      <Popup />
+      <CardSlider />
+    </>
+  );
 }
---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './popup.css'
+
+function Popup() {
+  return (
+    <div className='popup'>
+      <div className='offers'>
+        <h4>Limited time only</h4>
+        <h1>🔥Hurry up before ther offer ends🎉</h1>
+        <h3>Get exclusive offers, new product offers and other good stuff.</h3>
+        <button>X</button>
+      </div>
+            </div>
+  )
+}
+
+export default Popup
