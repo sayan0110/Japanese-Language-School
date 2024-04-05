7b7c7e7fb86697e2ceebb5f4446ce0b2f710c0fc
SAYAN MONDAL <smondal254842@gmail.com>
2024-04-05 11:37:40 +0530

    popup commit

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './popup.css'
 
 function Popup() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const popupClose = () => {
+    setIsOpen(false);
+  }
   return (
     <div className='popup'>
       <div className='offers'>
         <h4>Limited time only</h4>
         <h1>🔥Hurry up before ther offer ends🎉</h1>
         <h3>Get exclusive offers, new product offers and other good stuff.</h3>
-        <button>X</button>
+        <button onClick={popupClose}>X</button>
       </div>
             </div>
   )
